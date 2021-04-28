8f66eeb4682edc26d9d47797f708cf847da1e3cf
spampas <simone.pampaloni@gmail.com>
2021-04-28 17:35:11 +0200

    Update auctions_core.erl

--- erl/auctions_core.erl
+++ erl/auctions_core.erl
@@ -1,7 +1,7 @@
 -module(auctions_core).
 
--export([send_to_slaves/2]).
--export([compute_auction_state/2]).
+-export([send_to_slaves/3]).
+-export([compute_auction_state/2, select_winning_bids/2]).
 -export([create_auction/2, delete_auction/2, select_auction/2, auctions_list/1, auctions_agent_list/2, make_bid/2, delete_bid/2]).
 
 -record(state, {cluster, bully_state, leader}).
@@ -13,27 +13,32 @@
 %   AuctionData = {id_auction, id_agent, name, image, end_date, min_price, min_raise, sale_quantity} auction data
 %   BidList = [Bid1,...,BidM] list of all bids done for this auction
 %   AuctionState = [Bidx,...,Bidy] list of the actual winning bids
-%   Bid = {id_auction,id_user, timestamp,bid_value, quantity} a bid
+%   Bid = {id_bid, id_auction,id_user, timestamp,bid_value, quantity} a bid
 %---------------------------------------------------------------------------------------
 
 
-send_to_slaves(Cluster, Message) ->
-    Pids = utility:pids_from_global_registry("e_" ++ integer_to_list(Cluster)),
-    utility:send_message(Pids, Message).
+send_to_slaves(Cluster,Leader, Message) ->
+    if 
+        Leader == self() -> Pids = utility:pids_from_global_registry("e_" ++ integer_to_list(Cluster)), 
+                            utility:send_message(Pids, Message);
+        true -> ok
+    end.
 
 
-compute_auction_state(_,[]) -> [];
-compute_auction_state(SaleQuantity, [HeadSorted|T]) ->
+select_winning_bids(_,[]) -> [];
+select_winning_bids(SaleQuantity, [HeadSorted|T]) ->
     BidQuantity = element(5,HeadSorted),
     if 
-        SaleQuantity - BidQuantity > 0 -> [HeadSorted] ++ compute_auction_state(SaleQuantity-BidQuantity, T);
+        SaleQuantity - BidQuantity > 0 -> [HeadSorted] ++ select_winning_bids(SaleQuantity-BidQuantity, T);
         SaleQuantity - BidQuantity == 0 -> [HeadSorted];
-        SaleQuantity - BidQuantity < 0 -> NewHeadSorted =setelement(5, HeadSorted, SaleQuantity), [NewHeadSorted]
-    end;
+        true -> NewHeadSorted =setelement(5, HeadSorted, SaleQuantity), [NewHeadSorted]
+    end.
+
+
 compute_auction_state(AuctionData, BidList) ->
     SaleQuantity = element(8,AuctionData),
     SortedBidList = lists:reverse(lists:keysort(4, BidList)),
-    Result = compute_auction_state(SaleQuantity,SortedBidList),
+    Result = select_winning_bids(SaleQuantity,SortedBidList),
     Result.
 %-----------------------------------------------------------------------------------------------------
 
@@ -41,13 +46,13 @@ compute_auction_state(AuctionData, BidList) ->
 create_auction(Message, {State,Data}) ->
     {_, Id, NewAuction} = Message,
     NewData = Data ++ [{Id, {NewAuction,[],[]} }],
-    send_to_slaves(State#state.cluster, Message),
+    send_to_slaves(State#state.cluster,State#state.leader, Message),
     NewData.
 
 delete_auction(Message, {State,Data}) ->
     {_, Id, _} = Message,
     NewData = lists:keydelete(Id, 1, Data),
-    send_to_slaves(State#state.cluster, Message),
+    send_to_slaves(State#state.cluster,State#state.leader, Message),
     NewData.
 
 
@@ -62,9 +67,10 @@ make_bid(Message, {State,Data}) ->
     {AuctionData,BidList,_} = Auction,
     NewBidList = BidList ++ [NewBid],
     NewAuctionState = compute_auction_state(AuctionData, NewBidList),
+    io:format("NEW ACTION STATE ~p\n", [NewAuctionState]),
     NewAuction = {AuctionData,NewBidList,NewAuctionState},
     NewData = utility:modify_key_value_list(Data, {Id, NewAuction}),
-    send_to_slaves(State#state.cluster, Message),
+    send_to_slaves(State#state.cluster,State#state.leader, Message),
     {NewData, NewAuctionState}.
 
 delete_bid(Message, {State,Data}) ->
@@ -75,7 +81,7 @@ delete_bid(Message, {State,Data}) ->
     NewAuctionState = compute_auction_state(AuctionData, NewBidList),
     NewAuction = {AuctionData,NewBidList,NewAuctionState},
     NewData = utility:modify_key_value_list(Data, {Id, NewAuction}),
-    send_to_slaves(State#state.cluster, Message),
+    send_to_slaves(State#state.cluster,State#state.leader, Message),
     {NewData, NewAuctionState}.
 
 auctions_list([]) -> [];
@@ -85,10 +91,10 @@ auctions_list([HeadData|T]) ->
     [{Id,IdAgent,Name,EndDate}] ++auctions_list(T).
 
 auctions_agent_list([],_) -> [];
-auctions_agent_list([HeadData|T],[IdToFind]) ->
+auctions_agent_list([HeadData|T],IdToFind) ->
     {Id,{AuctionData, _, _}} = HeadData,
     {_,IdAgent,Name,_,EndDate,_,_,_} = AuctionData,
     if 
-        IdToFind == IdAgent -> [{Id,IdAgent,Name,EndDate}] ++auctions_list(T);
-        true -> [] ++ auctions_list(T)
+        IdToFind == IdAgent -> [{Id,IdAgent,Name,EndDate}] ++ auctions_agent_list(T, IdToFind);
+        true -> [] ++ auctions_agent_list(T, IdToFind)
     end.
