a61fd9212f249d6a5c4d5490b6de6db47ece7416
Nicola Ferrante <nicolaferrante96@gmail.com>
2021-05-21 22:33:47 +0200

    WebSocketServer

---
+++ DistributedAuctionSystem/WebSocketServer/src/main/java/it/unipi/dsmt/das/ws/endpoints/AuctionBackendEndpoint.java
@@ -0,0 +1,34 @@
+package it.unipi.dsmt.das.ws.endpoints;
+
+import it.unipi.dsmt.das.model.AuctionState;
+import it.unipi.dsmt.das.ws.decode.AuctionStateDecoder;
+import it.unipi.dsmt.das.ws.encode.AuctionStateEncoder;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(
+        value = "/auction_backend/{auction_id}",
+        encoders= {AuctionStateEncoder.class},
+        decoders= {AuctionStateDecoder.class})
+
+public class AuctionBackendEndpoint {
+    int auction;
+    @OnOpen
+    public void open(Session session, @PathParam("auction_id") int auctionId){
+        auction=auctionId;
+    }
+
+    @OnMessage
+    public void onMessage(Session session, AuctionState state){
+        AuctionEndpoint.updateAuction(auction, state);
+    }
+
+    @OnClose
+    public void close(Session session, @PathParam("auction_id") int auctionId){
+    }
+
+    @OnError
+    public void handleError(){};
+}
---
+++ DistributedAuctionSystem/WebSocketServer/src/main/java/it/unipi/dsmt/das/ws/endpoints/AuctionEndpoint.java
@@ -0,0 +1,67 @@
+package it.unipi.dsmt.das.ws.endpoints;
+
+import it.unipi.dsmt.das.ejbs.interfaces.AuctionStatePublisher;
+import it.unipi.dsmt.das.model.AuctionState;
+import it.unipi.dsmt.das.ws.decode.AuctionStateDecoder;
+import it.unipi.dsmt.das.ws.encode.AuctionStateEncoder;
+import javax.ejb.EJB;
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+
+@ServerEndpoint(
+        value = "/auction/{auction_id}",
+        encoders= {AuctionStateEncoder.class},
+        decoders= {AuctionStateDecoder.class})
+
+public class AuctionEndpoint {
+    public static final Map<Integer, Queue<Session>> subscribers = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void open(Session session, @PathParam("auction_id") int auctionId){
+        subscribers.putIfAbsent(auctionId, new ConcurrentLinkedQueue<>());
+        subscribers.get(auctionId).add(session);
+    }
+
+/**    @OnMessage
+    public void onMessage(Session session, Message message){
+
+        if(message instanceof BidMessage);
+        //TODO handle bid message
+        //sendToAll(id, subscribers[Id])
+        else if(message instanceof SubscriptionMessage);
+        //TODO handle subscription message
+
+        //Default do nothing
+    }
+**/
+    //TODO implement methods for message handling
+
+    @OnClose
+    public void close(Session session, @PathParam("auction_id") int auctionId){
+        Queue<Session> subs = subscribers.get(auctionId);
+        if (subs == null)
+            return;
+        subs.remove(session);
+    }
+
+    @OnError
+    public void handleError(){};
+
+    public static void closeAuction(int id){
+        subscribers.get(id).forEach(session -> {
+                session.getAsyncRemote().sendText("END");
+        });
+    };
+
+    public static void updateAuction(int id, AuctionState state){
+        subscribers.get(id).forEach( session -> {
+                session.getAsyncRemote().sendObject(state);
+        });
+    };
+}
